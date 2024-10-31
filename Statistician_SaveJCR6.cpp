34703fc0791baafa2883439d10fbd9750f17a8a7
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2024-10-31 12:57:25 +0100

    using

--- Statistician_SaveJCR6.cpp
+++ Statistician_SaveJCR6.cpp
@@ -1,25 +1,32 @@
-// Lic:
-// Statistician/Statistician_SaveJCR6.cpp
-// Statistician - Save to JCR6
-// version: 23.11.21
-// Copyright (C) 2023 Jeroen P. Broks
-// This software is provided 'as-is', without any express or implied
-// warranty.  In no event will the authors be held liable for any damages
-// arising from the use of this software.
-// Permission is granted to anyone to use this software for any purpose,
-// including commercial applications, and to alter it and redistribute it
-// freely, subject to the following restrictions:
-// 1. The origin of this software must not be misrepresented; you must not
-// claim that you wrote the original software. If you use this software
-// in a product, an acknowledgment in the product documentation would be
-// appreciated but is not required.
-// 2. Altered source versions must be plainly marked as such, and must not be
-// misrepresented as being the original software.
-// 3. This notice may not be removed or altered from any source distribution.
-// EndLic
+// License:
+// 	Statistician/Statistician_SaveJCR6.cpp
+// 	Statistician - Save to JCR6
+// 	version: 24.10.31
+// 
+// 	Copyright (C) 2023, 2024 Jeroen P. Broks
+// 
+// 	This software is provided 'as-is', without any express or implied
+// 	warranty.  In no event will the authors be held liable for any damages
+// 	arising from the use of this software.
+// 
+// 	Permission is granted to anyone to use this software for any purpose,
+// 	including commercial applications, and to alter it and redistribute it
+// 	freely, subject to the following restrictions:
+// 
+// 	1. The origin of this software must not be misrepresented; you must not
+// 	   claim that you wrote the original software. If you use this software
+// 	   in a product, an acknowledgment in the product documentation would be
+// 	   appreciated but is not required.
+// 	2. Altered source versions must be plainly marked as such, and must not be
+// 	   misrepresented as being the original software.
+// 	3. This notice may not be removed or altered from any source distribution.
+// End License
+
 #include "Statistician_SaveJCR6.hpp"
 
 #define SSSJNE(A) if (asblock) JCS = Block->nb(dir+A, "Statician", "Set up with Statician"); else JCS = D->nb(dir+A, storage, "Statistician", "Set up with Statistician");
+
+using namespace Slyvina::Units;
 
 namespace Slyvina {
 	namespace Statistician {
